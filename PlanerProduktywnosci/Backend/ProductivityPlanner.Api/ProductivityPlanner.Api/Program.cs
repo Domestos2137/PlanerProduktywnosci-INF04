f0a33ecc08af917b0cfd481ca9fb0590196455cc
Domestos2137 <bmichal2008@gmail.com>
2026-05-06 21:21:28 +0200

    Wykonanie pierwszej migracji
    Inicjalizacja bazy danych

--- PlanerProduktywnosci/Backend/ProductivityPlanner.Api/ProductivityPlanner.Api/Program.cs
+++ PlanerProduktywnosci/Backend/ProductivityPlanner.Api/ProductivityPlanner.Api/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+using ProductivityPlanner.Api.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite("Data Source=planner.db"));
 var app = builder.Build();

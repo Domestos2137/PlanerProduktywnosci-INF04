bf538e545fb512a9bde593bc43d9e7ffed327a7d
Domestos2137 <bmichal2008@gmail.com>
2026-05-06 20:51:47 +0200

    Stworzenie AppDbContext i poprawki w program.cs
    Połączenie z SQLite

--- PlanerProduktywnosci/Backend/ProductivityPlanner.Api/ProductivityPlanner.Api/Program.cs
+++ PlanerProduktywnosci/Backend/ProductivityPlanner.Api/ProductivityPlanner.Api/Program.cs
@@ -6,7 +6,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite("Data Source=planner.db"));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.

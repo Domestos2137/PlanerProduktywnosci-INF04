804b3e8dd0193d63ec49454ba0da5ab4da29e626
Domestos2137 <bmichal2008@gmail.com>
2026-05-07 13:40:48 +0200

    Dodanie routingu

--- PlanerProduktywnosci/Web/src/app/app.routes.ts
+++ PlanerProduktywnosci/Web/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { TaskListComponent } from './components/task-list/task-list'; // Upewnij się, że ścieżka pasuje
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: TaskListComponent }, // Strona główna
+  // Tutaj w przyszłości dodasz np. { path: 'login', component: LoginComponent }
+];
--- PlanerProduktywnosci/Web/src/app/app.ts
+++ PlanerProduktywnosci/Web/src/app/app.ts
@@ -1,12 +1,13 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet], // To musi tu być!
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('Web');
+  title = 'Planer Produktywności';
 }

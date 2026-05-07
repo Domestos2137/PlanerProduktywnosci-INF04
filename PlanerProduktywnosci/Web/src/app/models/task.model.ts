f3a4174e3db43afbcc31a8f34d51c0868440ac62
Domestos2137 <bmichal2008@gmail.com>
2026-05-07 12:00:16 +0200

    Dodanie modelu TodoTask i konfiguracja http

---
+++ PlanerProduktywnosci/Web/src/app/models/task.model.ts
@@ -0,0 +1,10 @@
+export interface TodoTask {
+  id?: number;
+  title: string;
+  description: string;
+  createdAt: Date;
+  dueDate?: Date;
+  status: string;
+  priority: string;
+  category: string;
+}

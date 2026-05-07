f03d9d1fa12b964461295c3c1a9539586faf3106
Domestos2137 <bmichal2008@gmail.com>
2026-05-07 22:26:25 +0200

    Dodano auth-guard

--- PlanerProduktywnosci/Web/src/app/app.routes.ts
+++ PlanerProduktywnosci/Web/src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { TaskListComponent } from './components/task-list/task-list'; // Upewnij się, że ścieżka pasuje
 import { LoginComponent } from './components/login/login';
-
+import { authGuard } from './guards/auth-guard';
+import { RegisterComponent } from './components/register/register';
 export const routes: Routes = [
-  { path: '', component: TaskListComponent }, // Strona główna
+  { path: '', component: TaskListComponent, canActivate: [authGuard] },
   { path: 'login', component: LoginComponent },
-  // Tutaj w przyszłości dodasz np. { path: 'login', component: LoginComponent }
+  { path: 'register', component: RegisterComponent }
 ];

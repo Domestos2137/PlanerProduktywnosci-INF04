1866ca00279e74efc17986e44c7aea1636d9cbf5
Domestos2137 <bmichal2008@gmail.com>
2026-05-07 12:54:36 +0200

    Wyswietlanie listy zadan z backendu
    Podstawa pod robienie dalszego UI i funkcjonalności

--- PlanerProduktywnosci/Web/src/app/components/task-list/task-list.ts
+++ PlanerProduktywnosci/Web/src/app/components/task-list/task-list.ts
@@ -1,9 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common'; 
+import { TaskService } from '../../services/task.service';
+import { TodoTask } from '../../models/task.model';
 
 @Component({
   selector: 'app-task-list',
-  imports: [],
+  standalone: true, 
+  imports: [CommonModule], 
   templateUrl: './task-list.html',
-  styleUrl: './task-list.scss',
+  styleUrl: './task-list.scss'
 })
-export class TaskList {}
+export class TaskListComponent implements OnInit {
+  tasks: TodoTask[] = [];
+
+  constructor(private taskService: TaskService) {}
+
+  ngOnInit(): void {
+    this.taskService.getTasks().subscribe({
+      next: (data) => {
+        this.tasks = data;
+        console.log('Pobrano zadania:', data);
+      },
+      error: (err) => {
+        console.error('Błąd pobierania zadań:', err);
+      }
+    });
+  }
+}

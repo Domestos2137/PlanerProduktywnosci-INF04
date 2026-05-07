8003097a713130091e31e05b0b56192ceeabd7dd
Domestos2137 <bmichal2008@gmail.com>
2026-05-07 20:46:22 +0200

    Dodanie widoku login

--- PlanerProduktywnosci/Web/src/app/app.routes.ts
+++ PlanerProduktywnosci/Web/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { TaskListComponent } from './components/task-list/task-list'; // Upewnij się, że ścieżka pasuje
+import { LoginComponent } from './components/login/login';
 
 export const routes: Routes = [
   { path: '', component: TaskListComponent }, // Strona główna
+  { path: 'login', component: LoginComponent },
   // Tutaj w przyszłości dodasz np. { path: 'login', component: LoginComponent }
 ];

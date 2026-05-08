25fa5ddf47efd516c7cb3cecc072125ea49ff210
Domestos2137 <bmichal2008@gmail.com>
2026-05-08 12:01:28 +0200

    Stworzenie logiki dla login window

--- PlanerProduktywnosci/Desktop/ProductivityPlanner.Desktop/LoginWindow.xaml.cs
+++ PlanerProduktywnosci/Desktop/ProductivityPlanner.Desktop/LoginWindow.xaml.cs
@@ -1,27 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Newtonsoft.Json;
+using System.Net.Http;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace ProductivityPlanner.Desktop
 {
-    /// <summary>
-    /// Logika interakcji dla klasy LoginWindow.xaml
-    /// </summary>
     public partial class LoginWindow : Window
     {
+        private readonly HttpClient _client = new HttpClient();
+
         public LoginWindow()
         {
             InitializeComponent();
+        }
+
+        private async void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            var loginData = new
+            {
+                Username = txtUsername.Text,
+                Password = txtPassword.Password
+            };
+
+            var json = JsonConvert.SerializeObject(loginData);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            try
+            {
+                var response = await _client.PostAsync("http://localhost:5290/api/auth/login", content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var token = await response.Content.ReadAsStringAsync();
+
+                    MainWindow mainWindow = new MainWindow(token);
+                    mainWindow.Show();
+                    this.Close(); 
+                }
+                else
+                {
+                    lblStatus.Text = "Błędny login lub hasło!";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Błąd połączenia: " + ex.Message);
+            }
         }
     }
 }

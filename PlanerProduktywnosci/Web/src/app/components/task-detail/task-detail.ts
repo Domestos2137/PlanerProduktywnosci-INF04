a1a3d9248f3e3b065348f2ec230334968da368a5
Domestos2137 <bmichal2008@gmail.com>
2026-05-09 11:29:37 +0200

    bugfix finalny
    W pełni działają już szczegóły i edycja

--- PlanerProduktywnosci/Web/src/app/components/task-detail/task-detail.ts
+++ PlanerProduktywnosci/Web/src/app/components/task-detail/task-detail.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core'; // 1. DODANO ChangeDetectorRef
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { TaskService } from '../../services/task.service';
-import { TodoTask } from '../../models/task.model'; // Upewnij się, że to Twoja ścieżka do modelu!
+import { TodoTask } from '../../models/task.model';
 
 @Component({
   selector: 'app-task-detail',
@@ -12,12 +12,13 @@ import { TodoTask } from '../../models/task.model'; // Upewnij się, że to Twoj
 })
 export class TaskDetailComponent implements OnInit {
   task?: TodoTask;
-  isLoading: boolean = true; // Dodajemy flagę ładowania
+  isLoading: boolean = true;
 
   constructor(
     private route: ActivatedRoute,
     private taskService: TaskService,
-    private router: Router
+    private router: Router,
+    private cdr: ChangeDetectorRef // 2. DODANO DO KONSTRUKTORA
   ) {}
 
   ngOnInit() {
@@ -26,17 +27,19 @@ export class TaskDetailComponent implements OnInit {
     if (id) {
       this.taskService.getTaskById(id).subscribe({
         next: (data) => {
-          console.log('Pobrano szczegóły:', data);
           this.task = data;
           this.isLoading = false;
+          this.cdr.detectChanges(); // 3. SZTURCHAMY WIDOK!
         },
         error: (err) => {
-          console.error('Błąd pobierania szczegółów:', err);
+          console.error('Błąd:', err);
           this.isLoading = false;
+          this.cdr.detectChanges(); // 3. SZTURCHAMY WIDOK!
         }
       });
     } else {
       this.isLoading = false;
+      this.cdr.detectChanges();
     }
   }
 
--- PlanerProduktywnosci/Web/src/app/components/task-edit/task-edit.ts
+++ PlanerProduktywnosci/Web/src/app/components/task-edit/task-edit.ts
@@ -1,18 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core'; // 1. DODANO
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { TaskService } from '../../services/task.service';
-import { TodoTask } from '../../models/task.model';
+import { TodoTask } from '../../models/task.model'; 
 
 @Component({
   selector: 'app-task-edit',
   standalone: true,
   imports: [CommonModule, FormsModule, RouterModule],
-  templateUrl: './task-edit.html'
+  templateUrl: './task-edit.html' 
 })
 export class TaskEditComponent implements OnInit {
-  // Inicjalizujemy pusty obiekt, żeby Angular nie rzucał błędami przed załadowaniem danych
   task: TodoTask = {
     title: '',
     description: '',
@@ -25,7 +24,8 @@ export class TaskEditComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private taskService: TaskService,
-    private router: Router
+    private router: Router,
+    private cdr: ChangeDetectorRef // 2. DODANO DO KONSTRUKTORA
   ) {}
 
   ngOnInit() {
@@ -34,10 +34,10 @@ export class TaskEditComponent implements OnInit {
       this.taskService.getTaskById(id).subscribe({
         next: (data) => {
           this.task = data;
-          // Jeśli data z API przychodzi jako string, konwertujemy ją dla inputa typu 'date'
           if (this.task.dueDate) {
             this.task.dueDate = new Date(this.task.dueDate);
           }
+          this.cdr.detectChanges(); // 3. SZTURCHAMY WIDOK!
         },
         error: (err) => console.error('Błąd ładowania zadania', err)
       });
@@ -45,14 +45,14 @@ export class TaskEditComponent implements OnInit {
   }
 
   saveTask() {
-      if (this.task.id) {
-        this.taskService.updateTask(this.task.id, this.task).subscribe({
-          next: () => {
-            alert('Zadanie zostało zaktualizowane!');
-            this.router.navigate(['/']); 
-          },
-          error: (err) => alert('Błąd podczas zapisu: ' + err.message)
-        });
-      }
+    if (this.task.id) {
+      this.taskService.updateTask(this.task.id, this.task).subscribe({
+        next: () => {
+          alert('Zadanie zostało zaktualizowane!');
+          this.router.navigate(['/']);
+        },
+        error: (err) => alert('Błąd podczas zapisu: ' + err.message)
+      });
     }
+  }
 }

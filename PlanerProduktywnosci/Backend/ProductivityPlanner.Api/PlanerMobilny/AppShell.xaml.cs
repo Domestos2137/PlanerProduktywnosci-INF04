d545ed1371788454e962b7d38b6c829374667bf9
Domestos2137 <bmichal2008@gmail.com>
2026-05-09 14:43:44 +0200

    Początek pracy nad mobilną

---
+++ PlanerProduktywnosci/Backend/ProductivityPlanner.Api/PlanerMobilny/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PlanerMobilny
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ PlanerProduktywnosci/Backend/ProductivityPlanner.Api/PlanerMobilny/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace PlanerMobilny
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
